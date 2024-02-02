b856d6538a7f34b8ff4571b9bfa041fee9e0cfe8
Tekkila <gordogamer8@gmail.com>
2024-02-02 18:56:16 -0300

    Entities Spawner

--- entity.py
+++ entity.py
@@ -1,11 +1,33 @@
-from typing import Tuple
+from __future__ import annotations
+import copy
+from typing import Tuple, TypeVar, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from maps.game_map import GameMap
+
+T = TypeVar("T", bound="Entity")
+
 
 class Entity:
-    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int]):
-        self.x = x
-        self.y = y
+    def __init__(
+            self,
+            char: str,
+            color: Tuple[int, int, int],
+            name: str = "<Unnamed>",
+            blocks_movement: bool = False,
+    ):
+
         self.char = char
         self.color = color
+        self.name = name
+        self.blocks_movement = blocks_movement
+
+    def spawn(self: T, gamemap: GameMap, x: int, y: int ) -> T:
+        clone = copy.deepcopy(self)
+        clone.x = x
+        clone.y = y
+        gamemap.entities.add(clone)
+        return clone
 
     def move(self, dx: int, dy: int) -> None:
         self.x += dx
